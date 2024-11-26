95eadea9bce86b4458b6e9b2b165348fe048602f
SeanQTpi <krgflamencreeper@gmail.com>
2024-11-26 20:18:31 +0800

    added initial files for each sections

---
+++ my-portfolio/app/partials/geninfo.tsx
@@ -0,0 +1,30 @@
+const Info = () => {
+
+  return (
+    <div className="border-solid border-2 border-white-600">
+      <h1 className="">Sean Gomez</h1>
+      <p>A student aspiring to be full-stack web developer. </p>
+      <p>
+        Hello, I'm Sean Patrick A. Gomez, 4th year Information Technology
+        student at La Consolacion College Manila with a strong foundation in
+        front-end and back-end web development. I aim to create innovative and
+        cutting edge solutions through technology, with a focus on web
+        development and sustainable IT practices. my adaptability and
+        persistence drive me t ocontinously improve my technical skills and
+        contribute meaningfully to the tech industry. Nice to meet you and
+        welcome to my personal portfolio.
+      </p>
+    </div>
+  );
+};
+
+const GenInfo = () => {
+  return (
+    <div>
+      <Info />
+      hello
+    </div>
+  );
+};
+
+export default GenInfo;
