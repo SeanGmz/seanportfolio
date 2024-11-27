b9b3478f78190892023491cfed3ce3b7b9af3cf5
SeanQTpi <krgflamencreeper@gmail.com>
2024-11-28 01:36:52 +0800

    added profile picture with effects in general information section

---
+++ my-portfolio/app/sections/components/pfp.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+import Image from "next/image";
+
+const ProfilePic = () => {
+  return (
+    <div className="w-full h-full relative">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{
+          opacity: 1,
+          transition: { delay: 0.5, duration: 0.3, ease: "easeIn" },
+        }}
+        className="z-10"
+      >
+        <div className="div w-[297px] h-[297px] xl:w-[427px] xl:h-[427px] absolute">
+          <Image
+            src="/resources/profile/pugi1.png"
+            className="object-contain"
+            quality={100}
+            fill
+            alt="Sean Gomez"
+          />
+        </div>
+      </motion.div>
+
+      {/* rotating effect */}
+      <motion.svg
+        className="w-[300px] h-[300px] xl:w-[430px] xl:h-[430px] z-20"
+        fill="transparent"
+        viewBox="0 0 516 516"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <motion.circle
+          cx="255"
+          cy="265"
+          r="252"
+          stroke="#fba087"
+          strokeWidth="4"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          initial={{strokeDasharray: "24 10 0 0"}}
+          animate={{strokeDasharray: ["15 120 25 25", "16 25 92 72", "4 250 22 22"],  rotate: [120, 360]}}
+          transition={{
+            duration: 20,
+            repeat: Infinity,
+            repeatType: "reverse",
+          }}
+        />
+      </motion.svg>
+    </div>
+  );
+};
+
+export default ProfilePic;
--- my-portfolio/app/sections/geninfo.tsx
+++ my-portfolio/app/sections/geninfo.tsx
@@ -1,7 +1,6 @@
-"use client";
-import { motion } from "framer-motion";
 
 import Link from "next/link";
+import ProfilePic from "./components/pfp";
 import { FaFacebook, FaGithub, FaLinkedin } from "react-icons/fa6";
 import { RiInstagramFill } from "react-icons/ri";
 import { FiDownload } from "react-icons/fi";
@@ -37,38 +36,34 @@ const Info = () => {
   };
 
   return (
-    <div className="border-solid border-2 xl:px-32 border-white-600 flex flex-col xl:flex-row items-center justify-between xl:pt-52 xl:pb-24">
-      <div className="text-center xl:text-left pt-10 pb-5 xl:py-0">
-        <h1 className="text-3xl mb-4 xl:text-6xl md:text-4xl sm:text-3xl font-bold">
+    <div className="border-solid border-2 xl:px-32 border-white-600 flex flex-col xl:flex-row items-center justify-between xl:pt-40 xl:pb-24">
+      <div className="text-center xl:text-left pt-10 pb-5 xl:py-0 order-2 xl:order-none">
+        <h1 className="text-3xl mb-4 xl:text-6xl md:text-4xl mx-3 md:mx-0 sm:text-3xl font-bold">
           Hi, <span>I'm Sean Gomez </span>
         </h1>
-        <p className="max-w-[500px] sm:text-sm md:text-md lg:text-base text-white/50">
+        <p className="max-w-[500px] sm:text-sm md:text-md mx-3 md:mx-0 lg:text-base text-white/50">
           A student aspiring to be full-stack web developer.
         </p>
 
         <button
           type="button"
-          className="mt-5 mx-auto xl:mx-0 xl:mt-5 flex flex-row items-center text-accent  hover:text-background hover:bg-accent hover:border-accent border-accent border-[2px] rounded-full px-8 py-3 transition-all ease-in duration-150"
+          className="mt-10 mx-auto xl:mx-0 xl:mt-5 flex flex-row items-center text-accent  hover:text-background hover:bg-accent hover:border-accent border-accent border-[2px] rounded-full px-8 py-3 transition-all ease-in duration-150"
         >
           <span className="m-0 mr-2 font-semibold">DOWNLOAD CV</span>
           <FiDownload className="text-lg text-white-500" />
         </button>
       </div>
 
-      <div className="flex flex-col xl:flex-row items-center">
-        <div className="">
+      <div className="w-0 xl:w-2/5"> </div>
+
+      <div className="order-3 xl:order-none">
           {/* socials component */}
           <Socials iconStyles="text-xl h-auto xl:mr-10 cursor-pointer border-accent border-[2px] p-2 my-0 mx-1.5 xl:mx-auto md:my-1.5 rounded-full hover:border-accent hover:bg-accent hover:text-background text-accent transition-all ease-in duration-150" />
         </div>
 
-        <div className="mx-auto">
-          <img
-            src="https://avatars.githubusercontent.com/u/69741071?v=4"
-            alt="Sean Gomez"
-            className="w-40 h-40 rounded-full"
-          />
+        <div className="mx-auto order-1 xl:order-none mt-20 mb-2 xl:my-0">
+          <ProfilePic />
         </div>
-      </div>
     </div>
   );
 };
