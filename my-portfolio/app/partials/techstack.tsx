b91ccd18defc597fc64af0f410f702261bc286a8
SeanQTpi <krgflamencreeper@gmail.com>
2024-11-27 15:22:27 +0800

    fixed margins and responsiveness of techstack section

--- my-portfolio/app/partials/techstack.tsx
+++ my-portfolio/app/partials/techstack.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { motion } from "motion/react"; // Import the framer-motion library
+import { motion } from "framer-motion"; // Import the framer-motion library
 
 export default function Techstack() {
   const marquee = [
@@ -20,24 +20,28 @@ export default function Techstack() {
 
   const Stack = () => {
     return (
-      <div>
-        <p>My Tech Stack</p>
+      <div className="mb-10 md:mb-20">
+        <h1 className="text-2xl sm:text-4xl text-center">My Tech Stack</h1>
       </div>
     );
   };
 
   const Uppermarquee = () => {
     return (
-      <div className="flex mx-20 mb-10 overflow-x-hidden marquee">
+      <div className="flex mx-5 xl:mx-20 mb-10 overflow-x-hidden marquee">
         <motion.div
           initial={{ x: 0 }}
           animate={{ x: "-100%" }}
-          transition={{ duration: 60, repeat: Infinity, ease: "linear" }}
+          transition={{ duration: window.innerWidth < 425 ? 20 : 60, repeat: Infinity, ease: "linear" }}
           className="flex flex-shrink-0"
         >
           {marquee.map((image, index) => {
             return (
-              <img src={image} key={index} className="w-24 mr-20" />
+              <img
+                src={image}
+                key={index}
+                className="w-9 mr-5 sm:w-20 sm:mr-20"
+              />
             );
           })}
         </motion.div>
@@ -45,12 +49,16 @@ export default function Techstack() {
         <motion.div
           initial={{ x: 0 }}
           animate={{ x: "-100%" }}
-          transition={{ duration: 60, repeat: Infinity, ease: "linear" }}
+          transition={{ duration: window.innerWidth < 425 ? 20 : 60, repeat: Infinity, ease: "linear" }}
           className="flex flex-shrink-0"
         >
           {marquee.map((image, index) => {
             return (
-              <img src={image} key={index} className="w-24 mr-20" />
+              <img
+                src={image}
+                key={index}
+                className="w-9 mr-5 sm:w-20 sm:mr-20"
+              />
             );
           })}
         </motion.div>
@@ -60,45 +68,57 @@ export default function Techstack() {
 
   const Lowermarquee = () => {
     return (
-      <div className="flex mx-20 overflow-x-hidden marquee">
+      <div className="flex mx-5 xl:mx-20 overflow-x-hidden marquee">
         <motion.div
           initial={{ x: "-100%" }}
           animate={{ x: 0 }}
-          transition={{ duration: 60, repeat: Infinity, ease: "linear" }}
+          transition={{
+            duration: window.innerWidth < 425 ? 20 : 60,
+            repeat: Infinity,
+            ease: "linear",
+          }}
           className="flex flex-shrink-0"
         >
           {marquee.toReversed().map((image, index) => {
-            return <img src={image} key={index} className="w-24 mr-20" />;
+            return (
+              <img
+                src={image}
+                key={index}
+                className="w-9 mr-5 sm:w-20 sm:mr-20"
+              />
+            );
           })}
         </motion.div>
 
         <motion.div
           initial={{ x: "-100%" }}
           animate={{ x: 0 }}
-          transition={{ duration: 60, repeat: Infinity, ease: "linear" }}
-          className="flex flex-shrink-0"
+          transition={{
+            duration: window.innerWidth < 425 ? 20 : 60, // Change duration based on screen size
+            repeat: Infinity,
+            ease: "linear",
+          }}
+          className="flex flex-shrink-0 "
         >
           {marquee.toReversed().map((image, index) => {
-            return <img src={image} key={index} className="w-24 mr-20" />;
+            return (
+              <img
+                src={image}
+                key={index}
+                className="w-9 mr-5 sm:w-20 sm:mr-20"
+              />
+            );
           })}
         </motion.div>
-      </div>
-    );
-  };
-
-  const Marquee = () => {
-    return (
-      <div className="py-12 border border-dashed border-red overflow-x-hidden">
-        <Stack />
-        <Uppermarquee />
-        <Lowermarquee />
       </div>
     );
   };
 
   return (
-    <div className="flex justify-center items-center border-solid border-2 border-red-600">
-      <Marquee />
-    </div>
+    <div className="py-20 md:my-24 border border-dashed border-red overflow-x-hidden">
+        <Stack />
+        <Uppermarquee />
+        <Lowermarquee />
+      </div>
   );
 }
