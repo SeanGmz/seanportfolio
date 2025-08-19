aa2f86b3a040691a30ec9910cd612ce1d2844797
SeanQTpi <krgflamencreeper@gmail.com>
2025-08-19 14:32:31 +0800

    changed app icon to custom icon

--- my-portfolio/app/layout.tsx
+++ my-portfolio/app/layout.tsx
@@ -21,6 +21,7 @@ export default function RootLayout({
     <html lang="en" className="scroll-smooth w-full">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
       </head>
       <body
         className=" antialiased border"
