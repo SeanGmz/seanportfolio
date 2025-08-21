ebb2172e382ef3be8f25200f57da4ad21d19f06b
SeanQTpi <krgflamencreeper@gmail.com>
2025-08-21 17:02:49 +0800

    created a separate file/UI for mobile view of the techstack section

---
+++ my-portfolio/components/elements/list.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { stackList } from "@/components/elements/stackItems";
+
+import {
+  Tooltip,
+  TooltipTrigger,
+  TooltipContent,
+  TooltipProvider,
+} from "@/components/ui/tooltip";
+
+export const List = () => {
+  return (
+    <div className="grid grid-cols-3 gap-y-2 gap-x-10 sm:gap-y-20 sm:gap-x-5 justify-center text-center items-center">
+      <TooltipProvider delayDuration={0}>
+        {...stackList.toReversed().map((tech, index) => (
+          <Tooltip key={index}>
+            <div className="justify-center flex">
+              <TooltipTrigger asChild>
+                <div className="group sm:px-10 px-8 sm:py-12 py-9 bg-secondary rounded-lg hover:bg-accent hover:text-[#18181b] text-accent transition ease-in-out duration-300 cursor-pointer transform-gpu will-change-transform hover:-translate-y-[2px]">
+                  <div className="relative pt-3 flex flex-col justify-center items-center w-6 h-6 sm:w-10 sm:h-10">
+                    <div className="text-[2.25rem] sm:text-[3.25rem] ">
+                      {tech.src}
+                    </div>
+                    <div className="mt-2 text-[0.60rem] sm:text-[0.75rem] whitespace-nowrap text-fore/70 group-hover:text-[#18181b] duration-300">
+                      {tech.title}
+                    </div>
+                  </div>
+                </div>
+              </TooltipTrigger>
+            </div>
+            <TooltipContent className="z-50 rounded-md transition-colors duration-200 bg-secondary text-xs px-3 py-2 md:text-md text-fore shadow-[0_-4px_6px_-1px_#18181b,0_2px_4px_-2px_#000]">
+              {tech.title}
+            </TooltipContent>
+          </Tooltip>
+        ))}
+      </TooltipProvider>
+    </div>
+  );
+};
