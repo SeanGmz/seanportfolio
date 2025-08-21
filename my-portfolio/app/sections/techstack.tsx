f0f229e32c44e04da71f0d37f37e6fce84dd3d90
SeanQTpi <krgflamencreeper@gmail.com>
2025-08-21 17:12:31 +0800

    removed unused import in techstack.tsx

--- my-portfolio/app/sections/techstack.tsx
+++ my-portfolio/app/sections/techstack.tsx
@@ -1,19 +1,16 @@
 "use client";
 import { List } from "@/components/elements/list";
-import { Uppermarquee, Lowermarquee } from "@/components/elements/maquee"; // Import the stackMarquee function
-import { useEffect } from "react";
+import { Uppermarquee, Lowermarquee } from "@/components/elements/maquee";
 
 const Techstack = () => {
-
   const DesktopView = () => {
     return (
       <div className="hidden md:block">
         <Uppermarquee />
         <Lowermarquee />
       </div>
-    )
-  }
-
+    );
+  };
   const MobileView = () => {
     return (
       <div className="block md:hidden mx-9">
@@ -22,13 +19,11 @@ const Techstack = () => {
     );
   };
 
-
   return (
     <div className="py-20 md:py-24 overflow-x-hidden">
       <div id="techstack" className="pb-10 md:pb-20">
         <h1 className="text-2xl sm:text-4xl text-center">My Tech Stack</h1>
       </div>
-      
       <DesktopView />
       <MobileView />
     </div>
