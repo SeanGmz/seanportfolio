bd2be36e5403b04044130b0adf50901ee4e69dc2
SeanQTpi <krgflamencreeper@gmail.com>
2025-08-21 17:01:28 +0800

    created a separate file for marquee/techstack items

---
+++ my-portfolio/components/elements/stackItems.tsx
@@ -0,0 +1,32 @@
+import {
+  FaHtml5,
+  FaPhp,
+  FaCss3Alt,
+  FaBootstrap,
+  FaNodeJs,
+} from "react-icons/fa6";
+
+import { SiTailwindcss, SiNextdotjs } from "react-icons/si";
+import { RiGithubFill, RiReactjsLine } from "react-icons/ri";
+import { FaGitAlt } from "react-icons/fa";
+import { GrMysql } from "react-icons/gr";
+import { LuFigma } from "react-icons/lu";
+import { IoLogoJavascript } from "react-icons/io5";
+import { BiLogoTypescript } from "react-icons/bi";
+
+export const stackList = [
+  { src: <FaHtml5 />, title: "HTML5" },
+  { src: <FaCss3Alt />, title: "CSS3" },
+  { src: <IoLogoJavascript />, title: "JavaScript" },
+  { src: <FaBootstrap />, title: "Bootstrap" },
+  { src: <SiTailwindcss />, title: "Tailwind CSS" },
+  { src: <BiLogoTypescript />, title: "TypeScript" },
+  { src: <RiReactjsLine />, title: "React.js" },
+  { src: <FaPhp />, title: "PHP" },
+  { src: <FaNodeJs />, title: "Node.js" },
+  { src: <GrMysql />, title: "MySQL" },
+  { src: <SiNextdotjs />, title: "Next.js" },
+  { src: <FaGitAlt />, title: "Git" },
+  { src: <RiGithubFill />, title: "GitHub" },
+  { src: <LuFigma />, title: "Figma" },
+];
